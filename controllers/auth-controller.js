afa90209379d5405a2998fb2a1df1f80f8f46e56
ROHIT <rohitupreti2002@gmail.com>
2024-06-05 13:34:24 +0530

    Made user-registration schema under model and data is saving in database written logic in auth-controller.js

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,17 +1,37 @@
+const User = require("../models/user-model.js");
+
 const home = async (req, res) => {
   try {
     res.status(200).send("Full Stack Development");
   } catch (error) {
-    res.status(400).send({ msg: "Page not found" });
+    res.status(500).send({ msg: "Internal Server Error" });
   }
 };
 
 const register = async (req, res) => {
+  //use validation using express-validator
   try {
     console.log(req.body);
-    res.status(200).json({ message: req.body });
+    const { username, email, phone, password } = req.body;
+
+    // Validate input
+    if (!username || !email || !phone || !password) {
+      return res.status(400).json({ msg: "All fields are required" });
+    }
+
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(400).json({ msg: "User already exists" });
+    }
+
+    const userCreate = await User.create({ username, email, phone, password });
+    res
+      .status(201)
+      .json({ msg: "User created successfully", user: userCreate });
   } catch (error) {
-    res.status(400).json("internal server error");
+    res
+      .status(500)
+      .json({ msg: "Internal Server Error", error: error.message });
   }
 };
 
---
+++ models/user-model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+//define the model or collection name and it is always save by default in mongoDB in lowercase.
+const User = new mongoose.model("User", userSchema);
+
+module.exports = User;
