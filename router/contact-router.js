fc09633677b4175d1e019b529617237f96737532
ROHIT <rohitupreti2002@gmail.com>
2024-06-18 23:38:40 +0530

    Increase contact limit

--- router/contact-router.js
+++ router/contact-router.js
@@ -7,7 +7,7 @@ const rateLimit = require("express-rate-limit");
 
 const contactLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 1,
+  max: 5,
   message: "Too many contact attempts from this IP, please try again later",
   handler: (_, res) => {
     res.status(429).json({
